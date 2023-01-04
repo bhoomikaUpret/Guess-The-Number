694174c1a2b97eef82ee20c2d11bad1be2c8ce02
bhoomikaUpret <112538577+bhoomikaUpret@users.noreply.github.com>
2023-01-04 19:18:30 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,8 @@
+// Create login Function
+function Login()
+{
+    player_name = document.getElementById("playername").value;
+    localStorage.setItem("player_name" , player_name);
+
+    window.location = "gamepage.html";
+}
